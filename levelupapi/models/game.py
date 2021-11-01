b940846908827e3c3f1a0f33e0831765b5b25ca4
CassieTesauro <cassie.tesauro@gmail.com>
2021-11-01 15:38:33 -0500

    first commit

---
+++ levelupapi/models/game.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Game(models.Model):
+    title = models.CharField(max_length=55)
+    maker = models.CharField(max_length=55)
+    gamer = models.CharField(max_length=12)
+    number_of_players = models.IntegerField()
+    skill_level =models.IntegerField()
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE)   
+
